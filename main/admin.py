97f0234699b66471ed8253389bd3199d1ddf6232
uncore <uncorepc49@gmail.com>
2024-11-04 21:00:19 +0500

    добавление моделей и регистрация их в админ. панель

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Profile, Page, PageImage
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'nickname', 'photo', 'date_created')
+    list_display_links = ('user',)
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'is_public', 'views', 'previous', 'date_created')
+    list_display_links = ('title',)
+
+
+class PageImageAdmin(admin.ModelAdmin):
+    list_display = ('image', 'date_uploaded')
+    list_display_links = ('image',)
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Page, PageAdmin)
+admin.site.register(PageImage, PageImageAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,51 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    nickname = models.CharField(max_length=50, verbose_name='Никнейм')
+    photo = models.ImageField(upload_to='profile_photos/', verbose_name='Фото')
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.nickname
+
+    class Meta:
+        verbose_name = 'профиль'
+        verbose_name_plural = 'Профили'
+        ordering = ['-date_created']
+
+
+class Page(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    previous = models.ForeignKey('Page', on_delete=models.CASCADE, null=True, blank=True,
+                                 verbose_name='Предыдущая страница')
+    icon = models.ImageField(upload_to='icons/', null=True, blank=True, verbose_name='Иконка')
+    title = models.CharField(max_length=50, verbose_name='Название')
+    is_public = models.BooleanField(default=False, verbose_name='Опубликована')
+    content = models.TextField(null=True, blank=True, verbose_name='Контент')
+    images = models.ManyToManyField('PageImage', null=True, blank=True, verbose_name='Изображения')
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    views = models.PositiveIntegerField(default=0, verbose_name='Количество просмотров')
+
+    def __str__(self):
+        return f'{self.user}: {self.title}'
+
+    class Meta:
+        verbose_name = 'страница'
+        verbose_name_plural = 'Страницы'
+        ordering = ['-date_created']
+
+
+class PageImage(models.Model):
+    image = models.ImageField(upload_to='page_images/', verbose_name='Изображение')
+    date_uploaded = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+
+    def __str__(self):
+        return self.image.name
+
+    class Meta:
+        verbose_name = 'изображение страницы'
+        verbose_name_plural = 'Изображения страниц'
+        ordering = ['-date_uploaded']
