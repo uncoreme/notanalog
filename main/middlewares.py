c0594fa623ed4d1fa0df49076c9b37737f6dca44
uncore <uncorepc49@gmail.com>
2024-11-09 01:38:35 +0500

    небольшие доработки

--- main/middlewares.py
+++ main/middlewares.py
@@ -2,8 +2,14 @@ from main.models import Profile, Page
 
 
 def profile(request):
-    return {'profile': Profile.objects.get(user=request.user)}
+    user_profile = Profile.objects.filter(user=request.user)
+    if user_profile.exists():
+        return {'profile': user_profile.first()}
+    return {}
 
 
 def pages(request):
-    return {'pages': Page.objects.filter(user=request.user)}
+    user_pages = Page.objects.filter(user=request.user)
+    if user_pages.exists():
+        return {'pages': user_pages}
+    return {}
